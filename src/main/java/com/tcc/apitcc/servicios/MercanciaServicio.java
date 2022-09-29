0d70c64817cc69cb5fcb90a8ec37de9b090cb1ba
308 <308@academicos.cesde.edu.co>
2022-09-29 12:06:26 -0500

    Se crea api rest en spring boot

---
+++ src/main/java/com/tcc/apitcc/servicios/MercanciaServicio.java
@@ -0,0 +1,13 @@
+package com.tcc.apitcc.servicios;
+
+public interface MercanciaServicio {
+
+    // Consultar mercancias
+
+    // Consultar mercancia por iup
+
+    // Guardar mercancia
+
+    // Eliminar mercancia 
+
+}
